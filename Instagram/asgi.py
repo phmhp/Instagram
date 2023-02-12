6bad2c9f84a1d0653350c1a31a051c89d16875fd
phmhp <phm6318@sookmyung.ac.kr>
2023-02-13 01:52:13 +0900

    [FIX] Fix jsondecodeerror

--- Instagram/asgi.py
+++ Instagram/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Instagram_coding project.
+ASGI config for Instagram project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Instagram_coding.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Instagram.settings')
 
 application = get_asgi_application()
--- Instagram/urls.py
+++ Instagram/urls.py
@@ -1,4 +1,4 @@
-"""Instagram_coding URL Configuration
+"""Instagram URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -15,10 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .veiws import Sub
 
 urlpatterns = [
-     path('admin/', admin.site.urls), #http://127.0.0.1:8000/andmin/ 일 경우 admin.site.urls가 실행된다는 뜻!
-     path("main/",Sub.as_view()) #http://127.0.0.1:8000/ 일때 뭘 실행시킬 것이냐? => views.py가 실행되도록 Sub.as_view() => #Sub클래스를 view로 사용하겠다는 뜻
-
+    path('admin/', admin.site.urls),
 ]
