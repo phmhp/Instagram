8546f0bcc91b1fe206909bccab01786f13135273
phmhp <phm6318@sookmyung.ac.kr>
2023-02-13 02:07:27 +0900

    [Add] Add content folder, user folder

--- Instagram/settings.py
+++ Instagram/settings.py
@@ -48,6 +48,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'content',
+    'user',
+    'Instagram'
 ]
 
 MIDDLEWARE = [
