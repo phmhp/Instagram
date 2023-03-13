531e5ef6e8b0c9ee08e9ed1fac92e7528d4d9f92
phmhp <phm6318@sookmyung.ac.kr>
2023-03-13 23:56:42 +0900

    [MODIFY] Modify login action

--- user/views.py
+++ user/views.py
@@ -30,4 +30,20 @@ class Login(APIView):
         return render(request,"user/login.html")
     def post(self,request):
         #Todo 로그인
-        pass
+        email = request.data.get('email', None)
+        password = request.data.get('password', None)
+
+        #filter에서는 쿼리셋을 리턴하기때문에, 결과가 하나든 여러개든 리스트 형태로 반환을 하게 됨.
+        # 이 때, first()를 함으로써 여러개가 아니라 하나만 리턴할 수 있는 것. (만약 두개있으면 첫번째 것만 리턴) => 이렇게 함으로써 user객체에 바로 접근이 가능. (ex- user.email이나 user.password형태로 사용 가능.)
+        user=User.objects.filter(email=email).first()
+
+        #만약 해당 email에 대한 회원이 없다면
+        if user is None:
+            return Response(status=400, data=dict(message="회원정보가 잘못됐습니다."))
+
+        #check_password(password)를 해서 만약 맞으면 True, 틀리면 False를 반환할 것.
+        if user.check_password(password):
+            #TODO 로그인이 성공했다면-> 세션 or 쿠키에 넣는다.
+            return Response(status=200)
+        else: #로그인에 실패했다면
+            return Response(status=400, data=dict(message="회원정보가 잘못됐습니다."))
